c2efc06049bf157f4ddd3ed32bb83459b8f0a4e0
Lucas Reyna <lucas.reyna143@gmail.com>
2022-04-11 13:24:14 -0700

    tests for containsNumbers function

--- sample-functions.test.js
+++ sample-functions.test.js
@@ -44,3 +44,40 @@ test('Testing div -- divide by zero error', () => {
     expect(target).toThrow(Error);
     expect(target).toThrow(/Divide By Zero/);
 });
+
+// Testing ContainsNumbers Function
+test('Testing constainsNumbers -- true', () => {
+    const result = myFunctions.containsNumbers("21, 7");
+    expect(result).toBeTruthy();
+});
+
+test('Testing constainsNumbers -- false', () => {
+    const result = myFunctions.containsNumbers("alkdfjdklafj");
+    expect(result).toBeFalsy();
+});
+
+test('Testing constainsNumbers -- num in middle', () => {
+    const result = myFunctions.containsNumbers("alkdfjd9klafj");
+    expect(result).toBeTruthy();
+});
+
+test('Testing constainsNumbers -- special characters', () => {
+    const result = myFunctions.containsNumbers("œ∑´®†¥¨ˆøπåß∂ƒ©≈ç√∫˜µ≤≥÷åß∂ƒ©˚¬…æ«‘πøˆ¥†´∑œ`¡™£¢∞§¶•ªº–≠");
+    expect(result).toBeFalsy();
+});
+
+// Bug in Program thinks some characteres are numbers
+test('Testing constainsNumbers -- space', () => {
+    const result = myFunctions.containsNumbers("a a");
+    expect(result).toBeTruthy();
+});
+
+test('Testing constainsNumbers -- tabs', () => {
+    const result = myFunctions.containsNumbers("a\ta");
+    expect(result).toBeTruthy();
+});
+
+test('Testing constainsNumbers -- new line', () => {
+    const result = myFunctions.containsNumbers("a\na");
+    expect(result).toBeTruthy();
+});
