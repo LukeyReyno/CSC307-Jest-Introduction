baabb82eeb559adecca1d31d91a26479956bcc4f
Lucas Reyna <lucas.reyna143@gmail.com>
2022-04-15 13:43:55 -0700

    added remove stock and check shares

--- stock-portfolio.js
+++ stock-portfolio.js
@@ -3,19 +3,45 @@ function createStockPortfolio()
   let sp = 
   {
     tickers: [],
-    numStocks: 0
+    numUniqueStocks: 0
   };
   return sp;
 }
 
-function addStock(portfolio, stockName)
+function addStock(portfolio, stockName, numshares)
 {
-  if ((portfolio.tickers.find((ticker) => ticker == stockName)) == undefined)
+  if ((portfolio.tickers.find((ticker) => ticker.name == stockName)) == undefined)
   {
-    portfolio.tickers.push(stockName)
-    portfolio.numStocks++;
+    portfolio.tickers.push({name: stockName, num: numshares});
+    portfolio.numUniqueStocks++;
   }
+}
+
+function removeStock(portfolio, stockName, numshares)
+{
+  const tickerIndex = portfolio.tickers.findIndex((ticker) => ticker.name == stockName);
+  if (tickerIndex != -1)
+  {
+    portfolio.tickers[tickerIndex].num -= numshares;
+    if (portfolio.tickers[tickerIndex].num <= 0)
+    {
+      portfolio.tickers.splice(tickerIndex, 1);
+      portfolio.numUniqueStocks--;
+    }
+  }
+}
+
+function checkShares(portfolio, stockName)
+{
+  const tickerIndex = portfolio.tickers.findIndex((ticker) => ticker.name == stockName);
+  if (tickerIndex != -1)
+  {
+    return portfolio.tickers[tickerIndex].num;
+  }
+  return 0;
 }
 
 exports.createStockPortfolio = createStockPortfolio;
 exports.addStock = addStock;
+exports.removeStock = removeStock;
+exports.checkShares = checkShares;
--- stock-portfolio.test.js
+++ stock-portfolio.test.js
@@ -2,15 +2,56 @@ const stockFunctions = require('./stock-portfolio.js');
 
 test('Testing stock portfolio -- creation', () => {
   const result = stockFunctions.createStockPortfolio();
-  expect(result.numStocks).toBe(0);
+  expect(result.numUniqueStocks).toBe(0);
   expect(result.tickers).toStrictEqual([]);
 });
 
-test('Test unique stock number', () =>
+test('Test add stock number', () =>
 {
   let sp = stockFunctions.createStockPortfolio();
-  stockFunctions.addStock(sp, "GME");
-  stockFunctions.addStock(sp, "RBLX");
-  expect(sp.numStocks).toBe(2);
+  stockFunctions.addStock(sp, "GME", 5);
+  stockFunctions.addStock(sp, "RBLX", 10);
+  expect(sp.numUniqueStocks).toBe(2);
 });
 
+test('Test remove stock number', () =>
+{
+  let sp = stockFunctions.createStockPortfolio();
+  stockFunctions.addStock(sp, "GME", 7);
+  stockFunctions.addStock(sp, "YUM", 11);
+  expect(sp.numUniqueStocks).toBe(2);
+
+  stockFunctions.removeStock(sp, "GME", 7);
+  stockFunctions.removeStock(sp, "YUM", 1);
+
+  expect(sp.numUniqueStocks).toBe(1);
+});
+
+test('Test lookup stockshares by stockname', () =>
+{
+  let sp = stockFunctions.createStockPortfolio();
+  stockFunctions.addStock(sp, "GME", 7);
+  stockFunctions.addStock(sp, "YUM", 11);
+
+  let result = stockFunctions.checkShares(sp, "GME");
+  expect(result).toBe(7);
+
+  result = stockFunctions.checkShares(sp, "YUM");
+  expect(result).toBe(11);
+
+  stockFunctions.removeStock(sp, "GME", 6);
+  stockFunctions.removeStock(sp, "YUM", 1);
+
+  result = stockFunctions.checkShares(sp, "GME");
+  expect(result).toBe(1);
+
+  result = stockFunctions.checkShares(sp, "YUM");
+  expect(result).toBe(10);
+
+  stockFunctions.removeStock(sp, "GME", 1);
+  result = stockFunctions.checkShares(sp, "GME");
+  expect(result).toBe(0);
+
+  result = stockFunctions.checkShares(sp, "YUM");
+  expect(result).toBe(10);
+});
